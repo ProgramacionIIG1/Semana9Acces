4d89892b8e1341edf2d356953625d59d1da69664
ERICK MAURICIO TIZNADO RODRIGUEZ <USIS038521@ugb.edu.sv>
2021-09-27 10:05:50 -0600

    Agregar archivos de proyecto.

---
+++ Semana9Acces/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+
+//Nathaly Milena Zelaya Caballero
+
+namespace Semana9Acces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\DELL\\Documents\\Usuario.accdb");
+            try
+            {
+                con.Open();
+                MessageBox.Show("La conexion fue exitosa");
+            }
+            catch (OleDbException ex )
+            {
+                MessageBox.Show("Error en la conexion", ex.ToString());
+            }
+
+        }
+    }
+}
